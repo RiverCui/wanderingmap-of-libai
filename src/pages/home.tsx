648273a85c3877331d7581692891d80cf272b400
River <moonquakes7@gmail.com>
2021-08-04 02:22:33 +0800

    修改边框，实现box1内间隙，添加top&bottom，整合代码

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import './home.scss';
 import mainBg from '../images/back.png';
 import {Chart1} from '../components/chart-1';
 import {Chart2} from '../components/chart-2';
+import {Chart3} from '../components/chart-3';
 
 export const Home = () => {
   return (
@@ -11,11 +12,10 @@ export const Home = () => {
         <section className="section1">
           <Chart1/>
           <Chart2/>
+          <Chart3/>
         </section>
         <section className="bordered section2"></section>
         <section className="bordered section3"></section>
-        <section className="bordered section4"></section>
-        <section className="bordered section5"></section>
       </main>
     </div>
   );
